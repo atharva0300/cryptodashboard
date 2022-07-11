da8d15b300844e7a63e7a705f112094bf40e1219
= <atharva0300@gmail.com>
2022-07-11 20:51:27 +0530

    added cryptoDetails page

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import HTMLReactParser from 'html-react-parser';
+import { useParams } from 'react-router-dom';
+import millify from 'millify';
+import { Col , Row, Typography, Select} from 'antd';
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+
+import { useGetCryptoDetailsQuery } from '../services/cryptoApi';
+
+// destructuring the Typography
+const { Title, Text} = Typography;
+const { Option } = Select;
 
 function CryptoDetails() {
+  const { coinId} = useParams();
+  console.log(coinId);
+  const {timePeriod, setTimePeriod} = useState('7d');
+  const {data , isFetching} = useGetCryptoDetailsQuery(coinId);
+  //getting all the data of a specific crypto coin
+
+  console.log(data);
+  console.log('coinId : ' , coinId);
+  if(isFetching){
+    return 'Loading...';
+  }
   return (
-    <div>CryptoDetails</div>
+    <div>CryptoDetails {coinId} </div>
   )
 }
 
--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -47,8 +47,8 @@ function Cryptocurrencies({simplified}) {
       <Row gutter = {[32 , 32]} className= "crypto-card-container">
         {cryptos?.map((currency) => (
           <Col xs = {24} sm = {12} lg = {6} className = "crypto-card" key = {currency.id}>
-            <Link to = {`/crypto/${currency.id}`}>
-              <Card title = {`${currency.rank}. ${currency.name}`} extra = {<img className='crypto-image' src = {currency.iconUrl} />} hoverable>
+            <Link to = {`/crypto/${currency.rank}`}>
+              <Card title = {`${currency.id}. ${currency.name}`} extra = {<img className='crypto-image' src = {currency.iconUrl} />} hoverable>
                 <p>Price : {millify(currency.price)}</p>
                 <p>Price : {millify(currency.marketCap)}</p>
                 <p>Price : {millify(currency.change)}%</p>
